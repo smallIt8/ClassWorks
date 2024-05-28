5294987a088cd6945a68986c1e9278c6d349bcc0
smallIt8 <smallIt88@gmail.com>
2024-05-28 12:30:53 +0300

    1

---
+++ src/Lesson3/Main.java
@@ -0,0 +1,25 @@
+package core.Lesson3;
+
+public class Main {
+    private static int x;
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        System.out.println(x);
+//        switchChanel();
+        int x = 345; // шаг 1
+        int y = 836; // шаг 2
+        int number = sum(x, y); // шаг 3
+
+        System.out.println("Результат сложения: " + number); // шаг 6
+    }
+//    public  static  void switchChanel() {
+//        System.out.println("канал переключен!");
+
+    public  static  int sum(int x, int y) {
+        return x + y;
+    }
+
+
+}
