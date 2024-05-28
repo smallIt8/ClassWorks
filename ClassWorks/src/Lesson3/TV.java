2e9e1f2911c78f55fcea8cf33c9b1d08ae29399f
smallIt8 <smallIt88@gmail.com>
2024-05-28 11:09:40 +0300

    1

---
+++ ClassWorks/src/Lesson3/TV.java
@@ -0,0 +1,23 @@
+package core.Lesson3;
+
+public class TV {
+
+    private String name;
+    private int countChanel;
+
+    public void switchChanel() {
+        System.out.println("|канал переключен");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
